da60f347902a2f62a6cdb682eba8446c41fe50d9
Dustin Roten <dustin.roten@utah.edu>
2022-04-23 18:29:16 -0600

    purge for a redo

--- r/src/notify.r
+++
@@ -1,8 +0,0 @@
-notify <- function(user.email = NULL, subject.text = NULL, body.text = NULL) {
-  
-  system(paste0("echo", " '", body.text, "' ", "|", " mail -s", " '", subject.text, "' ", user.email))
-  
-}
-
-
-
